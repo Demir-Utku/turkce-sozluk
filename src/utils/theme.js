1bb888f328d1e0fd17fcb54a6d7e4ab8bb887e0b
Demir-Utku <utkdmr1@gmail.com>
2021-05-09 00:58:56 +0300

    Add colors to theme.js file to use in the components

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,7 +2,17 @@ const space = [];
 
 const colors = {
   red: '#E11E3C',
-  gray: '#758291',
+  softRed: '#F8F8F8',
+  light: '#E8EAED',
+  compoundWordDark: '#2E494C',
+  compoundWordMedium: '#73A5AA',
+  compoundWordLight: '#E8F0F1',
+  proverbDark: '#4F3822',
+  proverbMedium: '#BB8E62',
+  proverbLight: '#2E494C',
+  textDark: '#0A151F',
+  textMedium: '#485158',
+  textLight: '#758291',
 };
 
 const radii = {

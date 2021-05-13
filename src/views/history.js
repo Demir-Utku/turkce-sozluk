236db6a29f1cc3a7c5b2973ecaf4f6a0b9fa28ee
Demir-Utku <utkdmr1@gmail.com>
2021-05-13 13:22:31 +0300

    Change status bar content to dark and background color to white using useFocusEffect

--- src/views/history.js
+++ src/views/history.js
@@ -1,11 +1,21 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, StatusBar, Platform } from 'react-native';
+import Box from '../components/box';
+import SafeAreaView from 'react-native-safe-area-view';
+import { useFocusEffect } from '@react-navigation/native';
 
 function History() {
+  useFocusEffect(
+    React.useCallback(() => {
+      StatusBar.setBarStyle('dark-content');
+      Platform.OS === 'android' && StatusBar.setBackgroundColor('#ecf0f1');
+    }, []),
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>History</Text>
-    </View>
+    <Box as={SafeAreaView} flex={1}>
+      <Text>Arama Geçmişi</Text>
+    </Box>
   );
 }
 

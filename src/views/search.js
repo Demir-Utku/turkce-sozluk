4b5040d1bcec843e5891235a75986afdeb0e5965
Demir-Utku <utkdmr1@gmail.com>
2021-05-13 13:31:56 +0300

    Change the display of status bar and logo when the keyboard focus is changed

--- src/views/search.js
+++ src/views/search.js
@@ -1,24 +1,90 @@
 import * as React from 'react';
-import { Button } from 'react-native';
+import {
+  ImageBackground,
+  Platform,
+  StatusBar,
+  Text,
+  Animated,
+} from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
+import { useFocusEffect } from '@react-navigation/native';
 
 import Box from '../components/box';
 import SearchComponent from '../components/search';
 
 import { Logo } from '../components/icons';
 
-function Search({ navigation }) {
+import bg from '../assets/bg.jpg';
+
+function Search() {
+  const [heroHeight] = React.useState(new Animated.Value(285));
+  const [isSearchFocus, setSearchFocus] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isSearchFocus) {
+      Animated.timing(heroHeight, {
+        toValue: 52 + 32,
+        duration: 250,
+      }).start();
+    } else {
+      Animated.timing(heroHeight, {
+        toValue: 285,
+        duration: 250,
+      }).start();
+    }
+  }, [isSearchFocus, heroHeight]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      StatusBar.setBarStyle(isSearchFocus ? 'dark-content' : 'light-content');
+      Platform.OS === 'android' && StatusBar.setBackgroundColor('#020202');
+    }, [isSearchFocus]),
+  );
+
   return (
-    <Box>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Detail')}
-      />
-      <Box py={20}>
-        <Logo width={120} color="red" />
+    <Box as={SafeAreaView} bg={isSearchFocus ? 'softRed' : 'red'} flex={1}>
+      {/* Header */}
+      <Box
+        as={Animated.View}
+        position="relative"
+        zIndex={1}
+        height={heroHeight}
+      >
+        {!isSearchFocus && (
+          <Box
+            as={ImageBackground}
+            source={bg}
+            style={{ width: '100%', height: '100%' }}
+          >
+            {/* Logo */}
+            <Box flex={1} alignItems="center" justifyContent="center">
+              <Logo width={120} color="white" />
+            </Box>
+          </Box>
+        )}
+        {/* Search */}
+        <Box
+          position="absolute"
+          left={0}
+          bottom={isSearchFocus ? 0 : -42}
+          width="100%"
+          p={16}
+        >
+          <SearchComponent onChangeFocus={(status) => setSearchFocus(status)} />
+        </Box>
       </Box>
 
-      <Box p={10}>
-        <SearchComponent />
+      {/* Content */}
+      <Box flex={1} bg="white" pt={isSearchFocus ? 0 : 26}>
+        {isSearchFocus ? (
+          <Box p={30} flex={1}>
+            <Text>Geçmiş</Text>
+          </Box>
+        ) : (
+          <Box p={30} flex={1}>
+            <Text>Öneri</Text>
+          </Box>
+        )}
       </Box>
     </Box>
   );

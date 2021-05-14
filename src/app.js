fda3d902afece5fe81486d15206a04b5f63cd47d
Demir-Utku <utkdmr1@gmail.com>
2021-05-14 18:31:55 +0300

    Add splash screen on the initialization of the app using react-native-splash-screen package

--- src/app.js
+++ src/app.js
@@ -1,45 +1,21 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components';
-
-import Search from './views/search';
-import Favorite from './views/favorite';
-import History from './views/history';
-import Detail from './views/detail';
-import TabBar from './components/tab-bar';
+import SplashScreen from 'react-native-splash-screen';
 
 import theme from './utils/theme';
-
-const Tab = createBottomTabNavigator();
-const HomeStack = createStackNavigator();
-
-function SearchStack() {
-  return (
-    <HomeStack.Navigator headerMode="none">
-      <HomeStack.Screen name="Search" component={Search} />
-      <HomeStack.Screen name="Detail" component={Detail} />
-    </HomeStack.Navigator>
-  );
-}
+import Navigation from './navigation';
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
-        <NavigationContainer>
-          <Tab.Navigator
-            initialRouteName="Search"
-            tabBar={(props) => <TabBar {...props} />}
-          >
-            <Tab.Screen name="History" component={History} />
-            <Tab.Screen name="Search" component={SearchStack} />
-            <Tab.Screen name="Favorite" component={Favorite} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <Navigation />
       </SafeAreaProvider>
     </ThemeProvider>
   );
